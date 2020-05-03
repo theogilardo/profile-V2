2fd67550898bf3e8714e963a38bd41a4533d0d1d
theogilardo <gilardotheo@gmail.com>
2020-05-03 17:50:46 +0200

    contact info

--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@ $(document).ready(function() {
 
   // SCROLL BUTTONS
 
-  // $('.js--scroll-to-about').click(function() {
-  //   $('html', 'body').animate({scrollTop: $('js--section-about').offset().top}, 1000);
-  // });
-
-
   $('.js--scroll-to-about').click(function () {
      $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
      console.log('scrooooll');
@@ -25,7 +20,31 @@ $(document).ready(function() {
      $('html, body').animate({scrollTop: $('.js--section-projects').offset().top}, 1000);
   });
 
+  // NAVIGATION ANIMATION
 
+   $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '60%'
+    });
+
+   $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '70%'
+    });
+
+   $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '60%'
+    });
+
+   $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn');
+    }, {
+        offset: '60%'
+    });
 
 });
 
