40b33b2913285a20e15754121fc40b247bce1995
theogilardo <gilardotheo@gmail.com>
2020-05-06 16:11:24 +0200

    bug fixed contact

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
   });
 
   $('.js--scroll-to-contact').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-projects').offset().top}, 1000);
+     $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
   });
 
   // NAVIGATION ANIMATION
