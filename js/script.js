3dfe7750e620c0576494a7a920caad486d3e1555
theogilardo <gilardotheo@gmail.com>
2020-05-28 16:30:32 +0200

    js wp modifications

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   });
 
   $('.js--scroll-to-projects').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-projects').offset().top}, 1000);
+     $('html, body').animate({scrollTop: $('.js--section-work').offset().top}, 1000);
   });
 
   $('.js--scroll-to-contact').click(function () {
@@ -37,13 +37,13 @@ $(document).ready(function() {
    $('.js--wp-3').waypoint(function(direction) {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
-        offset: '60%'
+        offset: '70%'
     });
 
    $('.js--wp-4').waypoint(function(direction) {
         $('.js--wp-4').addClass('animated fadeIn');
     }, {
-        offset: '60%'
+        offset: '70%'
     });
 
 });
