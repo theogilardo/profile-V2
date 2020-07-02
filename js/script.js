033c303d15b49dca9de97cdd07eeefa8b370b9b9
theogilardo <gilardotheo@gmail.com>
2020-07-02 15:41:55 +0200

    update projects

--- js/script.js
+++ js/script.js
@@ -1,66 +1,86 @@
-about = document.getElementById('about')
-skills = document.getElementById('skills')
-projects = document.getElementById('projects')
-contact = document.getElementById('contact')
-checkbox = document.querySelector('.checkbox')
-menu = document.querySelector('.menu')
-menuNav = document.querySelector('.menu-nav')
+about = document.getElementById("about");
+skills = document.getElementById("skills");
+projects = document.getElementById("projects");
+contact = document.getElementById("contact");
+checkbox = document.querySelector(".checkbox");
+menu = document.querySelector(".menu");
+menuNav = document.querySelector(".menu-nav");
 
-about.addEventListener('click', menuToggle)
-skills.addEventListener('click', menuToggle)
-projects.addEventListener('click', menuToggle)
-contact.addEventListener('click', menuToggle)
-
+about.addEventListener("click", menuToggle);
+skills.addEventListener("click", menuToggle);
+projects.addEventListener("click", menuToggle);
+contact.addEventListener("click", menuToggle);
 
 function menuToggle() {
   checkbox.checked = false;
-  menuNav.classList.toggle('.hidden')
-};
+  menuNav.classList.toggle(".hidden");
+}
 
-$(document).ready(function() {
-
+$(document).ready(function () {
   // SCROLL BUTTONS
-  $('.js--scroll-to-about').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
+  $(".js--scroll-to-about").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-about").offset().top },
+      1000
+    );
   });
 
-  $('.js--scroll-to-skills').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-skills').offset().top}, 1000);
+  $(".js--scroll-to-skills").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-skills").offset().top },
+      1000
+    );
   });
 
-  $('.js--scroll-to-projects').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-work').offset().top}, 1000);
+  $(".js--scroll-to-projects").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-work").offset().top },
+      1000
+    );
   });
 
-  $('.js--scroll-to-contact').click(function () {
-     $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
+  $(".js--scroll-to-contact").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-contact").offset().top },
+      1000
+    );
   });
 
   // NAVIGATION ANIMATION
 
-   $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '60%'
-    });
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "60%",
+    }
+  );
 
-   $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeIn');
-    }, {
-        offset: '70%'
-    });
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeIn");
+    },
+    {
+      offset: "70%",
+    }
+  );
 
-   $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '70%'
-    });
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "70%",
+    }
+  );
 
-   $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated fadeIn');
-    }, {
-        offset: '70%'
-    });
-
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animated fadeIn");
+    },
+    {
+      offset: "70%",
+    }
+  );
 });
-
